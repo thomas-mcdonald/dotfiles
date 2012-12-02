9858d93b2a8dd483489c3333f733bbe9d7eb92f2
Thomas McDonald <tom@conceptcoding.co.uk>
2012-12-02 12:23:17 +0000

    Added some stuff to path

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,7 @@
 export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+
+
+export PATH=/usr/local/share/npm/bin:$PATH
+export PATH="/usr/local/heroku/bin:$PATH"
