b53790d8778f92db18ea04a07e27b0d43e46a75d
Thomas McDonald <thomas.mcdonald@ocado.com>
2014-07-28 14:51:49 +0100

    remove todo function

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,25 +1,4 @@
 autoload colors && colors
-# cheers, @ehrenmurdick
-# http://github.com/ehrenmurdick/config/blob/master/zsh/prompt.zsh
-
-# This keeps the number of todos always available the right hand side of my
-# command line. I filter it to only count those tagged as "+next", so it's more
-# of a motivation to clear out the list.
-todo(){
-  if $(which todo.sh &> /dev/null)
-  then
-    num=$(echo $(todo.sh ls +next | wc -l))
-    let todos=num-2
-    if [ $todos != 0 ]
-    then
-      echo "$todos"
-    else
-      echo ""
-    fi
-  else
-    echo ""
-  fi
-}
 
 # https://github.com/benhoskings/dot-files/blob/master/files/.zsh/prompt
 tilde_or_pwd() {
