23433be86206be1d316a2140a3ca75d5326989e1
Thomas McDonald <thomasm@palantir.com>
2015-08-03 14:25:30 -0700

    add gnb - alias for git checkout -b

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -6,7 +6,7 @@ then
   alias git=$hub_path
 fi
 
-# The rest of my fun git aliases
 alias gl='glog --graph'
 alias glog='git log --date-order --pretty="format:%C(yellow)%h%Cblue%d%Creset %s %C(white) %an, %ar%Creset"'
-alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
+alias gs='git status -sb'
+alias gnb='git checkout -b'
