e7140748edcc8b4bcdf19f4e9ac8560ef7dce983
Thomas McDonald <tom@conceptcoding.co.uk>
2020-12-25 23:49:31 +0000

    use native homebrew completions

--- zsh/fpath.zsh
+++ zsh/fpath.zsh
@@ -1,2 +1,7 @@
 #add each topic folder to fpath so that they can add functions and completion scripts
 for topic_folder ($ZSH/*) if [ -d $topic_folder ]; then  fpath=($topic_folder $fpath); fi;
+
+# load homebrew completions
+if type brew &>/dev/null; then
+  FPATH=$(brew --prefix)/share/zsh/site-functions:$FPATH
+fi
