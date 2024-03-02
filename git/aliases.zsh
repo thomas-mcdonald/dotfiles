badfe20c48918c62af5b33f157825c3404845eed
Thomas McDonald <thomas-mcdonald@users.noreply.github.com>
2024-03-02 14:40:23 +0800

    Remove hub alias

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,11 +1,3 @@
-# Use `hub` as our git wrapper:
-#   http://defunkt.github.com/hub/
-hub_path=$(which hub)
-if [[ -f $hub_path ]]
-then
-  alias git=$hub_path
-fi
-
 alias gl='glog --graph'
 alias glog='git log --date-order --pretty="format:%C(yellow)%h%Cblue%d%Creset %s %C(white) %an, %ar%Creset"'
 alias gs='git status -sb'
